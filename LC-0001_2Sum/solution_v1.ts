0b0ab1ee2319341131c1c9fe7520e828fbe1554b
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2026-02-24 17:44:29 +0200

    feat: Two Sum - Brute Force O(n^2) [#LC-0001]

--- LC-0001_2Sum/solution_v1.ts
+++ LC-0001_2Sum/solution_v1.ts
@@ -1,20 +1,33 @@
+/**
+ * Brute Force (Double Loop).
+ *
+ * Iterate over all unique index pairs (i < j)
+ * and return the first pair whose sum equals target.
+ *
+ * Time: O(n^2)
+ * Space: O(1) extra space
+ */
 function twoSum(nums: number[], target: number): number[] {
     let res: number[] = [];
-    let len = nums.length;
+    const len = nums.length;
+
     for (let i = 0; i < len - 1; i++) {
-        let n1 = nums[i]
+        const n1 = nums[i];
+
         for (let j = i + 1; j < len; j++) {
-            let n2 = nums[j]
+            const n2 = nums[j];
+
             if (n1 + n2 === target) {
-                res = [i, j]
+                // Return indices immediately once found
+                res = [i, j];
                 return res;
             }
         }
     }
-    
-    return res;
-};
 
-console.log(twoSum([2,7,11,15], 9));
+    return res;
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
 
 export {};
